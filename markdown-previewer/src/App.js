0f934b7cffc99fdba2039b57278039754f555492
Thomas Doherty <thomas_doherty92@hotmail.com>
2023-09-21 07:49:31 -0400

    added markdown previewer code from codepen. question on lines 33-34 line 45, played with markdown in setState

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {marked} from 'marked';
 
-function App() {
+function App() { 
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="top">
+            <h1 className="text-center">Markdown Previewer</h1>
+          </div>
+        </div>
+        <UserInput />
+      </div>
+    )
+}
+
+function UserInput() {
+  const [markdown, setMarkdown] = useState(`# Here is an example Markdown heading element\n ## You can edit or completely replace what's below\n <br> \n\n ### I used the hash key for a heading element and double / triple hash for h2/h3 respectively. \nHere's some *italics*, **bold text** and ~~strikethrough~~! \n\n 
+    A list for you:\n\n * Here is a link to some [news](https://www.bbc.com/news). \n * Here is some inline code: \`print('hello world')\`\n * A block quote: \n > "I think, therefore I am"\n \n \n 
+    Here's a code block... \n * And it would not be the internet without a cat picture:\n \n ![Cat Picture](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucepets.com%2Fall-about-tabby-cats-552489&psig=AOvVaw3OE5iGTojIje3NfC7lPUD_&ust=1685982804087000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLDk58KFqv8CFQAAAAAdAAAAABAF)`); 
+  
+  const updatePreview = (e) => {
+    setMarkdown(
+      e.target.value
+    );
+  }
+  
+  const createMarkup = () => {
+    return { __html: marked(markdown)}
+  }
+  
+// BELOW: updatePreview is meant to take a parameter (e) but doesn't need to  with onChange 
+// Does it assume the change is (e)?
+
+  console.log(markdown);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="col-md-7">
+        <h2>Enter Text</h2>
+        <textarea className="editor" 
+            id="editor" 
+            placeholder={markdown}
+            value={markdown} 
+            onChange={updatePreview}/>
+      </div>
+      <div className="col-md-7">
+        <h2>Output Markdown</h2>
+        <div className="preview" id="preview" dangerouslySetInnerHTML={createMarkup()}>
+        </div>
+      </div>  
     </div>
-  );
+    )
 }
 
 export default App;
